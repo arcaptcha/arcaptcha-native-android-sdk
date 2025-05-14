25db317cfe04801f8cb76913bfc4de5bd806a60d
sadeghb97 <sadeghb97@gmail.com>
2025-05-14 15:56:02 +0330

    up

--- app/src/main/java/ir/sbpro/arcaptchafast/CaptchaActivity.kt
+++ app/src/main/java/ir/sbpro/arcaptchafast/CaptchaActivity.kt
@@ -2,46 +2,115 @@ package ir.sbpro.arcaptchafast
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.Visibility
+import co.arcaptcha.arcaptcha_native_sdk.captchas.CaptchaView
+import co.arcaptcha.arcaptcha_native_sdk.captchas.ClassicCaptchaView
+import co.arcaptcha.arcaptcha_native_sdk.captchas.SlidePuzzleView
+import co.arcaptcha.arcaptcha_native_sdk.captchas.SoundCaptchaView
+import co.arcaptcha.arcaptcha_native_sdk.models.CaptchaCallback
+import co.arcaptcha.arcaptcha_native_sdk.models.CaptchaState
 import ir.sbpro.arcaptchafast.databinding.ActivityCaptchaBinding
 import co.arcaptcha.arcaptcha_native_sdk.remote.ArcaptchaAPI
 
 class CaptchaActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCaptchaBinding
+    protected lateinit var classicCaptcha: ClassicCaptchaView
+    protected lateinit var soundCaptcha: SoundCaptchaView
+    protected lateinit var slideCaptcha: SlidePuzzleView
+    protected lateinit var classicCaptchaStatus: TextView
+    protected lateinit var voiceChallengeStatus: TextView
+    protected lateinit var slidePuzzleStatus: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCaptchaBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        classicCaptcha = binding.classicCaptchaView
+        classicCaptchaStatus = binding.classicCaptchaStatus
+        soundCaptcha = binding.soundCaptchaView
+        voiceChallengeStatus = binding.voiceChallengeStatus
+        slideCaptcha = binding.slideCaptchaView
+        slidePuzzleStatus = binding.slidePuzzleStatus
+
         val questArcApi = ArcaptchaAPI("afge5xjsq6", "localhost")
         val puzzleArcApi = ArcaptchaAPI("bq44zwr6cn", "localhost")
 
-        val classicCaptcha = binding.classicCaptchaView
-        val soundCaptcha = binding.soundCaptchaView
-        val slideCaptcha = binding.slideCaptchaView
-
-        classicCaptcha.initCaptcha(questArcApi) {
-            Log.d("XQQQOutBoxOnCorrect", "HaBeleJan Classic")
-        }
-
-        /*classicCaptcha.initCaptcha(arcaptchaAPI, object : CaptchaCallback {
+        classicCaptcha.initCaptcha(questArcApi, object : CaptchaCallback {
             override fun onCorrectAnswer() {
-                Log.d("XQQQOutBoxOnCorrect", "HaBeleJan")
+                showStatus(classicCaptchaStatus, "پازل حل شد!")
             }
-        })*/
 
-        soundCaptcha.initCaptcha(questArcApi) {
-            Log.d("XQQQOutBoxOnCorrect", "HaBeleJan Sound")
-        }
+            override fun onError(message: String) {
+                finishStatus(classicCaptcha, classicCaptchaStatus, "مشکلی در لود کپچا بوجود آمده است!")
+            }
 
-        slideCaptcha.initCaptcha(puzzleArcApi) {
-            Log.d("XQQQOutBoxOnCorrect", "HaBeleJan Slide")
-        }
+            override fun onWrongAnswer() {
+                classicCaptcha.loadCaptcha()
+            }
+
+            override fun onStateChanged(state: CaptchaState) {
+                if(state == CaptchaState.LoadingCaptcha) hideStatus(classicCaptchaStatus)
+            }
+        })
+
+        soundCaptcha.initCaptcha(questArcApi, object : CaptchaCallback {
+            override fun onCorrectAnswer() {
+                showStatus(voiceChallengeStatus, "پازل حل شد!")
+            }
+
+            override fun onError(message: String) {
+                finishStatus(soundCaptcha, voiceChallengeStatus, "مشکلی در لود کپچا بوجود آمده است!")
+            }
+
+            override fun onWrongAnswer() {
+                soundCaptcha.loadCaptcha()
+            }
+
+            override fun onStateChanged(state: CaptchaState) {
+                if(state == CaptchaState.LoadingCaptcha) hideStatus(voiceChallengeStatus)
+            }
+        })
+
+        slideCaptcha.initCaptcha(puzzleArcApi, object : CaptchaCallback {
+            override fun onCorrectAnswer() {
+                showStatus(slidePuzzleStatus, "پازل حل شد!")
+            }
+
+            override fun onError(message: String) {
+                finishStatus(slideCaptcha, slidePuzzleStatus, "مشکلی در لود کپچا بوجود آمده است!")
+            }
+
+            override fun onWrongAnswer() {
+                slideCaptcha.loadCaptcha()
+            }
+
+            override fun onStateChanged(state: CaptchaState) {
+                if(state == CaptchaState.LoadingCaptcha) hideStatus(slidePuzzleStatus)
+            }
+        })
 
         classicCaptcha.loadCaptcha()
         soundCaptcha.loadCaptcha()
         slideCaptcha.loadCaptcha()
+    }
+
+    fun finishStatus(captcha: CaptchaView, tv: TextView, message: String){
+        captcha.visibility = View.GONE
+        tv.setText(message)
+        tv.visibility = View.VISIBLE
+    }
+
+    fun showStatus(tv: TextView, message: String){
+        tv.setText(message)
+        tv.visibility = View.VISIBLE
+    }
+
+    fun hideStatus(tv: TextView){
+        tv.visibility = View.GONE
     }
 }
