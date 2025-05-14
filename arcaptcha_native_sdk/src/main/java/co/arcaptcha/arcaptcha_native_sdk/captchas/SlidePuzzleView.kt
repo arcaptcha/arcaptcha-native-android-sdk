5a3e5848ba78d6e8e8402ceebcb1888946a012b8
sadeghb97 <sadeghb97@gmail.com>
2025-05-14 13:46:16 +0330

    finishing get user inputs in slide puzzles

--- arcaptcha_native_sdk/src/main/java/co/arcaptcha/arcaptcha_native_sdk/captchas/SlidePuzzleView.kt
+++ arcaptcha_native_sdk/src/main/java/co/arcaptcha/arcaptcha_native_sdk/captchas/SlidePuzzleView.kt
@@ -1,5 +1,6 @@
 package co.arcaptcha.arcaptcha_native_sdk.captchas
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
@@ -13,67 +14,58 @@ import co.arcaptcha.arcaptcha_native_sdk.models.SlidePuzzleCallback
 import co.arcaptcha.arcaptcha_native_sdk.models.captchas.SlidePuzzleData
 import com.bumptech.glide.Glide
 import android.view.MotionEvent
+import android.view.View
+import co.arcaptcha.arcaptcha_native_sdk.components.PuzzleSlider
 
+@SuppressLint("ClickableViewAccessibility")
 class SlidePuzzleView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
 ) : CaptchaView(context, attrs), SlidePuzzleCallback {
+    companion object {
+        const val puzzleOriginalWidth = 260
+        const val puzzleOriginalHeight = 160
+    }
+
     override val manager = SlidePuzzleManager(this)
     val slidePuzzleView = binding.slideCaptcha
     override val captchaBox: LinearLayout = slidePuzzleView.captchaBox
     private var puzzleBgImage: ImageView = slidePuzzleView.puzzleBgImage
     private var puzzlePieceImage: ImageView = slidePuzzleView.puzzlePieceImage
-    private var sliderContainer: RelativeLayout = slidePuzzleView.sliderContainer
-    private var sliderThumb: ImageView = slidePuzzleView.sliderThumb
+    private var puzzleSlider: PuzzleSlider = slidePuzzleView.puzzleSlider
+    private var puzzleWidth = 0
+    private var puzzleHeight = 0
 
     init {
         toggleButton.setImageResource(R.drawable.ic_image)
         orientation = VERTICAL
 
-        sliderThumb.setOnTouchListener { v, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    // این خط خیلی مهمه!
-                    sliderContainer.parent.requestDisallowInterceptTouchEvent(true)
-                    true
-                }
+        puzzleSlider.sliderMaxValue = puzzleOriginalWidth.toFloat()
+        puzzleSlider.setOnInputListener {realValue, scaledValue ->
+            puzzlePieceImage.translationX = realValue
+        }
 
-                MotionEvent.ACTION_MOVE -> {
-                    val location = IntArray(2)
-                    sliderContainer.getLocationOnScreen(location)
-                    val containerX = location[0]
-
-                    val containerWidth = sliderContainer.width
-                    val maxX = containerWidth - sliderThumb.width
-
-                    val newX = (event.rawX - containerX - sliderThumb.width / 2)
-                        .coerceIn(0f, maxX.toFloat())
-
-                    sliderThumb.translationX = newX
-                    puzzlePieceImage.translationX = newX
-
-                    val value = (newX / maxX * 260).toInt()
-                    Log.d("XQQQSSliderValue", "Current Value: $value")
-                    true
-                }
-
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    // پایان تعامل
-                    sliderContainer.parent.requestDisallowInterceptTouchEvent(false)
-                    v.performClick()
-                    true
-                }
-
-                else -> false
-            }
+        puzzleSlider.setOnChangeListener {realValue, scaledValue, dd, ddt ->
+            Log.d("XQQQSSUserAnsReady", "($scaledValue, $dd)")
         }
     }
 
     override fun onCaptchaLoaded(data: SlidePuzzleData) {
         Log.d("XQQQStateSlide", "onCaptchaLoaded: ${data.captcha_type}, ${data.status}")
+
         if(data.content?.puzzle_background != null && data.content.puzzle_piece != null){
             val puzzleBgUrl = arcaptchaApi.getOriginalImageUrl(data.content.puzzle_background)
             val puzzlePieceUrl = arcaptchaApi.getOriginalImageUrl(data.content.puzzle_piece)
+            val puzzlePieceY = data.content.y ?: 0
+
+            //hatman bad az visible shodane aks ha anjam shavad
+            puzzleBgImage.post {
+                val location = IntArray(2)
+                puzzleBgImage.getLocationOnScreen(location)
+                puzzleWidth = puzzleBgImage.width
+                puzzleHeight = puzzleBgImage.height
+                puzzlePieceImage.translationY = ((puzzleHeight.toFloat() / puzzleOriginalHeight) * puzzlePieceY)
+            }
 
             Glide.with(context)
                 .load(puzzleBgUrl)
@@ -82,16 +74,16 @@ class SlidePuzzleView @JvmOverloads constructor(
             Glide.with(context)
                 .load(puzzlePieceUrl)
                 .into(puzzlePieceImage)
-
-            Log.d("XQQQStateSlideBGUrl", puzzleBgUrl)
-            Log.d("XQQQStateSlidePieceUrl", puzzlePieceUrl)
         }
         outerCallback?.onCaptchaLoaded()
     }
 
     override fun onStateChanged(state: CaptchaState) {
         when (state) {
-            CaptchaState.LoadingCaptcha, CaptchaState.SubmittingSolution -> loadingMode()
+            CaptchaState.LoadingCaptcha, CaptchaState.SubmittingSolution -> {
+                loadingMode()
+                puzzleSlider.resetSlider()
+            }
             CaptchaState.AwaitingUserInput -> contentMode()
             CaptchaState.Done -> {}
             CaptchaState.Error -> {}
---
+++ arcaptcha_native_sdk/src/main/java/co/arcaptcha/arcaptcha_native_sdk/components/PuzzleSlider.kt
@@ -0,0 +1,96 @@
+package co.arcaptcha.arcaptcha_native_sdk.components
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import co.arcaptcha.arcaptcha_native_sdk.databinding.PuzzleSliderViewBinding
+import kotlin.math.roundToInt
+
+
+@SuppressLint("ClickableViewAccessibility")
+class PuzzleSlider @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null
+) : RelativeLayout(context, attrs), View.OnTouchListener {
+    protected val binding: PuzzleSliderViewBinding =
+        PuzzleSliderViewBinding.inflate(LayoutInflater.from(context), this, true)
+    private var sliderContainer: RelativeLayout = binding.sliderContainer
+    private var sliderThumb: ImageView = binding.sliderThumb
+
+    public var startTime = 0L
+    public var sliderMaxValue = 0f
+    protected var sliderCurrentRealValue = 0f
+    protected var sliderCurrentScaledValue = 0
+    protected var onInput: ((Float, Int) -> Unit)? = null
+    protected var onChange: ((Float, Int, Int, Int) -> Unit)? = null
+
+    init {
+        sliderThumb.setOnTouchListener(this)
+    }
+
+    public fun setOnInputListener(l: (Float, Int) -> Unit){
+        onInput = l
+    }
+
+    public fun setOnChangeListener(l: (Float, Int, Int, Int) -> Unit){
+        onChange = l
+    }
+
+    fun resetSlider(){
+        startTime = 0L
+        sliderThumb.translationX = 0f
+        sliderCurrentRealValue = 0f
+        sliderCurrentScaledValue = 0
+    }
+
+    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        if(p0 == null || p1 == null) return false
+
+        return when (p1.action) {
+            MotionEvent.ACTION_DOWN -> {
+                startTime = System.currentTimeMillis()
+                sliderContainer.parent.requestDisallowInterceptTouchEvent(true)
+                true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                val location = IntArray(2)
+                sliderContainer.getLocationOnScreen(location)
+                val containerX = location[0]
+
+                val containerWidth = sliderContainer.width
+                val maxX = containerWidth - sliderThumb.width
+
+                val newX = (p1.rawX - containerX - sliderThumb.width / 2)
+                    .coerceIn(0f, maxX.toFloat())
+
+                sliderThumb.translationX = newX
+
+                sliderCurrentRealValue = newX
+                sliderCurrentScaledValue =
+                    if(sliderMaxValue > 0) (sliderCurrentRealValue / maxX * sliderMaxValue).roundToInt()
+                    else sliderCurrentRealValue.roundToInt()
+
+                onInput?.invoke(sliderCurrentRealValue, sliderCurrentScaledValue)
+                true
+            }
+
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                val dur = (System.currentTimeMillis() - startTime).toInt()
+                startTime = 0
+
+                sliderContainer.parent.requestDisallowInterceptTouchEvent(false)
+                p0.performClick()
+                onChange?.invoke(sliderCurrentRealValue, sliderCurrentScaledValue, dur, dur)
+                true
+            }
+
+            else -> false
+        }
+    }
+}
